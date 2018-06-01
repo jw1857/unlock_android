ae8928b6e59946156574a2d2a6a6c0de32d827a9
jameswinters <jw1857@york.ac.uk>
2018-06-01 17:12:52 +0100

    added bPOIActivity

---
+++ app/src/main/java/com/example/jameswinters/unlock_android/bPOI.java
@@ -0,0 +1,13 @@
+package com.example.jameswinters.unlock_android;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class bPOI extends POI {
+
+    public bPOI(){
+        this.setLockStatus(false);
+    }
+
+}
